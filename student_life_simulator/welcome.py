d496c2b137afacd90a8e7a60cce4105285250b78
Shreyaa1507 <144233524+Shreyaa1507@users.noreply.github.com>
2026-05-12 09:49:01 +0800

    Add welcome screen

---
+++ student_life_simulator/welcome.py
@@ -0,0 +1,10 @@
+BORDER = "=" * 50
+
+
+def display_welcome_screen():
+    """Display the sign-up welcome screen."""
+    print(BORDER)
+    print("|             * Student Simulator *              |")
+    print(BORDER)
+    print("|    * Enter your details below to sign up! *    |")
+    print(BORDER)
